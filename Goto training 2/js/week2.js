4625662645286405d612d4d5f66c885fff576612
Terence Williams <wterence73@gmail.com>
2014-11-05 20:07:18 -0500

    Asked the user for name and year born

--- Goto training 2/js/week2.js
+++ Goto training 2/js/week2.js
@@ -5,4 +5,22 @@ Goto training week 2
 11-05-2014
  */
 
-alert("testing 1, 2, 3");
+//alert("testing 1, 2, 3");
+
+//Create a js file that will tell us how old we are
+
+//Ask the user their name
+var name = prompt ("Please type in your name:");
+
+//Say hello to the user and let them know what we are doing
+//ALERT the user
+alert("Welcome "+name+"! Let's go ahead and figure out how old you are.");
+
+
+//ask the user what year they were born in
+//Create a variable to catch their answer
+
+var yearBorn = prompt("What year were you born");
+
+//Console.log out the response
+console.log(yearBorn);
