aa2d9c5ad5a4cf648733ddeea323aa202171f7fc
Terence Williams <wterence73@gmail.com>
2014-11-09 12:20:27 -0500

    Expressions Wacky 4

--- Expressions Assignment/Expressions_Wacky/js/wacky.js
+++ Expressions Assignment/Expressions_Wacky/js/wacky.js
@@ -24,7 +24,11 @@ console.log(name)
 //Alert user we're about to beginning of the exercise
 alert("Let's get off to an interesting exploration!");
 
+//Inform user of needed information
 
+alert("We will be using the formula distance= rate * time to start." + "Now, how far is the moon from the Earth?");
+
+alert("The moon is a whooping 238,900 miles!")
 
 
 
