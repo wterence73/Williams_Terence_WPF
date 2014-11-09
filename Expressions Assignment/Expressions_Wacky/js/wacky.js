22aa56b9a8683678a25ccad26ee0f13d7d291371
Terence Williams <wterence73@gmail.com>
2014-11-09 11:39:07 -0500

    Expressions Wacky 1
    Expressions

--- Expressions Assignment/Expressions_Wacky/js/wacky.js
+++ Expressions Assignment/Expressions_Wacky/js/wacky.js
@@ -7,4 +7,12 @@
 
 //Create a js with a wacky concept
 
-//
+//Create variable to involve user
+
+var wackyQue = prompt("How would you like to touch the moon?");
+
+//Console.log out the response
+console.log(wackyQue)
+
+
+
