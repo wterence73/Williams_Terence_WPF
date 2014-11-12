b3694f166c5c16879a9c18d36c8a2c782b010ead
Terence Williams <wterence73@gmail.com>
2014-11-12 18:30:38 -0500

    Conditionals_Industry update 2

--- Conditionals_Industry/js/conditionals_industry.js
+++ Conditionals_Industry/js/conditionals_industry.js
@@ -11,8 +11,19 @@ Terence Williams
 var welcomeMessage = prompt("Hello and welcome to Twill Mart. " + "What is your name?");
 console.log(welcomeMessage)
 
-//Alert customer that we have 
+//Console.log out the response
+console.log(welcomeMessage)
+
+
+//Alert customer that we have email updates
+alert("Our company has email alerts!");
 
 
 //Ask customer if they would like to sign up for email updates and sales
+
+var emails = prompt("Would you like to sign up?")
+
+//Console.log out the response
+console.log(emails)
+
 
