04aae11718863d8c9b345e017bf8774bf3cdcf46
Terence Williams <wterence73@gmail.com>
2014-11-12 18:01:32 -0500

    Conditionals_Industry update 1

---
+++ Conditionals_Industry/js/conditionals_industry.js
@@ -0,0 +1,13 @@
+/*
+Terence Williams
+ WPF 1408 Section 01
+ Expressions Assignment
+ 11-12-14
+ */
+
+//Web build addressing customer needs
+//welcome customer and ask for the name
+
+var welcomeMessage = prompt("Hello and welcome to Twill Mart " + "What is your name?");
+console.log(welcomeMessage)
+
