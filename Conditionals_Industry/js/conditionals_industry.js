75b61038f0df6d842dca34d92746359e81c02d5f
Terence Williams <wterence73@gmail.com>
2014-11-12 18:20:38 -0500

    Conditionals_Industry update 2

--- Conditionals_Industry/js/conditionals_industry.js
+++ Conditionals_Industry/js/conditionals_industry.js
@@ -8,6 +8,11 @@ Terence Williams
 //Web build addressing customer needs
 //welcome customer and ask for the name
 
-var welcomeMessage = prompt("Hello and welcome to Twill Mart " + "What is your name?");
+var welcomeMessage = prompt("Hello and welcome to Twill Mart. " + "What is your name?");
 console.log(welcomeMessage)
+
+//Alert customer that we have 
+
+
+//Ask customer if they would like to sign up for email updates and sales
 
