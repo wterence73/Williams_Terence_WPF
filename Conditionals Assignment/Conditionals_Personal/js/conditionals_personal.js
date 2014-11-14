a4c6e3bddb7e9c85aa57108a966b833d15c0733c
Terence Williams <wterence73@gmail.com>
2014-11-13 23:43:32 -0500

    Personal updates 2

---
+++ Conditionals Assignment/Conditionals_Personal/js/conditionals_personal.js
@@ -0,0 +1,19 @@
+/*
+ Terence Williams
+ WPF 1408 Section 01
+ Expressions Assignment
+ 11-12-14
+ */
+
+//Team NC Car Sales
+//Welcome the customer to the car lot
+alert("Welcome to the best car lot ever!");
+
+var carsAvailable = prompt("What make and model are you interested in?");
+
+//Console.log out the response
+console.log(carsAvailable);
+
+
+
+
--- Conditionals_Wacky/js/conditionals_wacky.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Twill007 on 11/12/14.
- */
