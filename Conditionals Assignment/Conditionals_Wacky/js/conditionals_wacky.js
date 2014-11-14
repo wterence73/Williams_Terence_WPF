fe840de0d373f6200da03be44aa6d73b72a13a37
Terence Williams <wterence73@gmail.com>
2014-11-14 00:32:19 -0500

    Wacky updates 2

--- Conditionals Assignment/Conditionals_Wacky/js/conditionals_wacky.js
+++ Conditionals Assignment/Conditionals_Wacky/js/conditionals_wacky.js
@@ -6,4 +6,15 @@
  */
 
 //What's the safest height to bungee jump
+//Welcome tourist to All American Bungee Jump
+
+alert("Welcome to the most exciting time of your life!");
+
+//Alert tourist with the height choices for the jump
+alert("We have any jump from 10 feet to 1000 feet jumps.");
+
+var bigJumps = prompt("How far would you like to jump?");
+
+//Console.log out the response
+console.log(bigjumps);
 
