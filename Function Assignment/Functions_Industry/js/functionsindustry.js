25d6920168c53478ab4f7534dee77c9fa862a835
Terence Williams <wterence73@gmail.com>
2014-11-19 17:50:48 -0500

    Industry update 1

--- Function Assignment/Functions_Industry/js/functionsindustry.js
+++ Function Assignment/Functions_Industry/js/functionsindustry.js
@@ -1,3 +1,15 @@
 /**
- * Created by Twill007 on 11/19/14.
+ Terence Williams
+ Functions Assignment
+ Industry
+ 11/19/2014
  */
+
+//Create a master file system
+//Alert the with company info
+
+alert("Track Destiny Filing Company");
+
+
+
+
