93a009893a14aa59e75bc6af5f0b4e6b65df1fd3
Terence Williams <wterence73@gmail.com>
2014-11-20 16:52:47 -0500

    Personal update 3

--- Function Assignment/Functions_Personal/js/functionspersonal.js
+++ Function Assignment/Functions_Personal/js/functionspersonal.js
@@ -1,8 +1,19 @@
 /**
  Terence Williams
  Functions Assignment
- Industry
+ Personal
  11/19/2014
  */
 
 //Create Banking Budget
+//Alert the budget is important
+
+alert ("A monthly budget is very important to keep.");
+
+//Create a variable for months
+
+var calMonths = 12
+
+//Alert with number of months
+alert ("There are 12 months in a year.");
+
