23f0eb0af353781a228fae3a58da6eecec6c0dca
Terence Williams <wterence73@gmail.com>
2014-11-19 20:56:28 -0500

    Industry update 8

--- Function Assignment/Functions_Industry/js/functionsindustry.js
+++ Function Assignment/Functions_Industry/js/functionsindustry.js
@@ -37,7 +37,18 @@ var numberFiles = 450;
 var sectionCount = 2;
 var sectionNumbers = 225;
 
-
+//If the number of files are equal to a number, print to console "All is accounted for."
+//If the files are total below 451
+if(numberFiles <= 450){
+   //All is accounted for.
+   console.log("All the files are completely accounted for.");
+}else if(numberFiles > sectionNumbers){
+   //The count is more
+   console.log("The total count is more than expected.");
+}else{
+   //There must be a recount
+   console.log("Please do a new count.");
+}
 
 
 
