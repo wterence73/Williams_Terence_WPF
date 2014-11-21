5e6fc52fb2f12584db7938603c1c9491f2a4a01c
Terence Williams <wterence73@gmail.com>
2014-11-20 23:41:11 -0500

    Wacky update 1

--- Function Assignment/Functions_Wacky/js/functionswacky.js
+++ Function Assignment/Functions_Wacky/js/functionswacky.js
@@ -1,3 +1,17 @@
 /**
- * Created by Twill007 on 11/19/14.
+ Terence Williams
+ Functions Assignment
+ Personal
+ 11/19/2014
  */
+
+//Create for three clown fish sizes
+
+//Alert for company with large clown fish
+
+alert("Patrick and Spongebob Fishery");
+
+var fishSize = prompt("Guess what is the largest fish ever?");
+
+//Console.log out the response
+console.log(fishSize);
