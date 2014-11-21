ed5bb26aba4f8ccc0c9faac6bf7c32941540edfa
Terence Williams <wterence73@gmail.com>
2014-11-20 21:56:16 -0500

    Personal update 8

--- Function Assignment/Functions_Personal/js/functionspersonal.js
+++ Function Assignment/Functions_Personal/js/functionspersonal.js
@@ -41,7 +41,16 @@ var billTwo = 560
 var billThree = 900
 var calIncome = 3000
 
+if(billOne * billThree == calIncome || billTwo * billOne / 2 == calIncome){
+  //Insufficient income
+console.log("There is not enough income!");
+}
 
-
-
+if(billOne + billThree < calIncome){
+  //Fair amount
+  console.log("We are good with amount of income.");
+}else(billTwo > billOne < calIncome){
+   //All is still good
+ console.log("We still have enough income available!");
+}
 
